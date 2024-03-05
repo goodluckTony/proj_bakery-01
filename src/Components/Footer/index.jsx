6af7ef5b7cf5cd80486a547a17c86bf5bdb8240a
L. Maksym <mlobacov@gmail.com>
2024-03-05 14:24:16 +0200

    test

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import './index.scss';
+import '../../assets/style/index.scss';
+import '../../assets/style/icon.scss';
 import FooterLogo from "../../assets/img/footer/footer-logo.svg";
 
 const Footer = () => {
