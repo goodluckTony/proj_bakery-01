d099b317962489b33c6c0567d53d4ca89780ac3c
L. Maksym <mlobacov@gmail.com>
2024-03-15 13:10:30 +0200

    test

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import './index.scss';
-import Logo from  '../../assets/img/header/logo-icon.svg';
+import React, { useState } from "react";
+import "./index.scss";
+import Logo from "../../assets/img/header/logo-icon.svg";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,9 +9,9 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
+  // const closeMenu = () => {
+  //   setIsMenuOpen(false);
+  // };
 
   return (
     <>
@@ -21,12 +21,24 @@ const Header = () => {
             <a className="header__logo" href="#">
               <img src={Logo} alt="logo" />
             </a>
-            <div className="menu__content menu__content--header" style={{ display: "none" }}>
+            <div className="menu__content menu__content--header">
               <nav className="menu__list">
                 <ul className="menu__items menu__items--header">
-                  <li><a className="menu__link" href="#bake">Products</a></li>
-                  <li><a className="menu__link" href="#about">About Us</a></li>
-                  <li><a className="menu__link" href="#">Blog</a></li>
+                  <li>
+                    <a className="menu__link" href="#bake">
+                      Products
+                    </a>
+                  </li>
+                  <li>
+                    <a className="menu__link" href="#about">
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a className="menu__link" href="#">
+                      Blog
+                    </a>
+                  </li>
                 </ul>
               </nav>
               <a className="menu__contact" href="tel:6295550129">
@@ -34,11 +46,15 @@ const Header = () => {
                 Contact us
               </a>
             </div>
-            <a className="icon icon--menu" onClick={toggleMenu} href="#menu"></a>
+            <a
+              className="icon icon--menu"
+              onClick={toggleMenu}
+              href="#menu"
+            ></a>
           </div>
         </div>
       </header>
-      <aside className={`page__menu ${isMenuOpen ? 'open' : ''}`} id="menu">
+      <aside className={`page__menu ${isMenuOpen ? "open" : ""}`} id="menu">
         <div className="menu__container">
           <div className="menu__top">
             <a className="menu__logo" href="#">
@@ -49,9 +65,21 @@ const Header = () => {
           <div className="menu__content">
             <nav className="menu__list">
               <ul className="menu__items" onClick={toggleMenu}>
-                <li><a className="menu__link" href="#bake">Products</a></li>
-                <li><a className="menu__link" href="#about">About Us</a></li>
-                <li><a className="menu__link" href="#">Blog</a></li>
+                <li>
+                  <a className="menu__link" href="#bake">
+                    Products
+                  </a>
+                </li>
+                <li>
+                  <a className="menu__link" href="#about">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a className="menu__link" href="#">
+                    Blog
+                  </a>
+                </li>
               </ul>
             </nav>
             <a className="menu__contact" href="tel:6295550129">
@@ -62,7 +90,7 @@ const Header = () => {
         </div>
       </aside>
     </>
-  )
-}
+  );
+};
 
 export default Header;
